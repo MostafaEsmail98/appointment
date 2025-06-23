05ce28e4db5bff6d52e3377867e1569bb7ddd863
mosta <mostafaelshrbeeny98@gmail.com>
2025-06-23 16:23:41 +0300

    add core files

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,40 @@
+import 'package:appointment/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      // case Routes.loginScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider(
+      //       create: (context) => getIt<LoginCubit>(),
+      //       child: const LoginScreen(),
+      //     ),
+      //   );
+      // case Routes.signUpScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider(
+      //       create: (context) => getIt<SignupCubit>(),
+      //       child: const SignupScreen(),
+      //     ),
+      //   );
+      // case Routes.homeScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider(
+      //       create: (context) => HomeCubit(getIt())..getSpecializations(),
+      //       child: const HomeScreen(),
+      //     ),
+      //   );
+      default:
+        return null;
+    }
+  }
+}
